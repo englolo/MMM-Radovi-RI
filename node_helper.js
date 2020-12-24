6e9da0809c838d7fbd67b9071e67a4c71e301930
lolo <69340216+englolo@users.noreply.github.com>
2020-12-24 16:56:51 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -9,7 +9,7 @@ const NodeHelper = require('node_helper');
 const puppeteer = require("puppeteer");
 const cheerio = require('cheerio');
 const request = require('request');
-
+var self;
 const url = 'http://www.ri-info.net/Radovi.aspx';
 
 var elecWorkList = [];
@@ -18,16 +18,24 @@ var waterWork = [];
 module.exports = NodeHelper.create({
 
     start: function () {
-        self = this; // ? dont know if this is correct but only like thet payload is not mix
+        self = this;
+        self.browser;
         console.log("Starting node_helper for: " + this.name);
     },
 
     socketNotificationReceived: function (notification, payload) {
         if (notification === 'GET_WORK_DATA') {
 
-            this.searchStreet = payload;
+            self.config = payload
+                console.log("Starting node_helper for: ", self.config),
+
+            self.streetUpper = self.config.street.toUpperCase()
+                self.searchStreet = self.streetUpper + ", " + self.config.place;
+
+            //console.log("Recived payload for search: " , self.searchStreet); //  //uncomment to see if you recive payload
+
+
             getData();
-            //console.log("Recived payload for search: " + this.searchStreet); //  //uncomment to see if you recive payload
         }
 
     },
@@ -35,106 +43,120 @@ module.exports = NodeHelper.create({
 });
 
 async function getData() {
-    const browser = await puppeteer.launch({
-        executablePath: 'chromium-browser'
-    });
+    try {
 
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.waitForTimeout(1000);
-    await page.click('.searchBtn1');
-    await page.waitForTimeout(1000);
+        if (self.config.chromiumPath != null) {
+            self.browser = await puppeteer.launch({
+                executablePath: self.config.chromiumPath,
+                headless: !self.config.showBrowser
+            }); // headless : false
+        } else {
+            self.browser = await puppeteer.launch({
+                headless: !self.config.showBrowser
+            }); // headless : false
+        }
 
-    const html = await page.content();
+        const page = await self.browser.newPage();
+        await page.goto(url);
+        await page.waitForTimeout(1000);
+        await page.click('.searchBtn1');
+        await page.waitForTimeout(1000);
 
-    const $ = cheerio.load(html);
-    var work=[];
-    var elec = [];
-    var water = [];
-    var elecList = [];
-    var waterList = [];
-    const elWater = $('#cphContent_cphSadrzaj_upVoda table tbody ')[0];
-    const elElec = $('#cphContent_cphSadrzaj_upStruja table tbody ')[0];
-    
-	
-    if (elElec) {
-        var elementElec = $('#cphContent_cphSadrzaj_upStruja table tbody tr td span').filter(function () { 
-            return $(this).text() === self.searchStreet;
+        const html = await page.content();
+
+        const $ = cheerio.load(html);
+        var work = [];
+        var elec = [];
+        var water = [];
+        var elecList = [];
+        var waterList = [];
+        const elWater = $('#cphContent_cphSadrzaj_upVoda table tbody ')[0];
+        const elElec = $('#cphContent_cphSadrzaj_upStruja table tbody ')[0];
+
+        if (elElec) {
+            var elementElec = $('#cphContent_cphSadrzaj_upStruja table tbody tr td span').filter(function () {
+                return $(this).text() === self.searchStreet;
             }).parent().parent();
             $(elementElec).each(function (jex, eEl) {
-            var note = "";
-            const $eEl = $(eEl);
-            let startDate = $eEl.prevAll('.datum').first().find('tr[class="datum"]>th>div').text().trim(); 
-            let street = $eEl.find('span[id*="Label2"]').text();
-            let streetNmbr = $eEl.find('span[id*="Label3"]').text();
-            let startTime = $eEl.find('span[id*="Label4"]').text();
-            let endTime = $eEl.find('span[id*="Label5"]').text();
-            let endWork = $eEl.find('span[id*="Label6"]').text();
-            let noteCheck = $eEl.children().children().first()[0];
-            
-	    if (noteCheck) {
-                var note = $eEl.nextUntil().next().first('.tableNapomena').find('td[class="tableNapomena"]>div>div').text().trim();
-            };
+                var note = "";
+                const $eEl = $(eEl);
+                let startDate = $eEl.prevAll('.datum').first().find('tr[class="datum"]>th>div').text().trim();
+                let street = $eEl.find('span[id*="Label2"]').text();
+                let streetNmbr = $eEl.find('span[id*="Label3"]').text();
+                let startTime = $eEl.find('span[id*="Label4"]').text();
+                let endTime = $eEl.find('span[id*="Label5"]').text();
+                let endWork = $eEl.find('span[id*="Label6"]').text();
+                let noteCheck = $eEl.children().children().first()[0];
 
-            var elec = {
-                startDate,
-                street,
-                streetNmbr,
-                startTime,
-                endTime,
-                endWork,
-                note
-            };
-            elecList.push(elec);
+                if (noteCheck) {
+                    var note = $eEl.nextUntil().next().first('.tableNapomena').find('td[class="tableNapomena"]>div>div').text().trim();
+                };
 
-        });
-        //console.log("New entries for electrical!", elecList);     //uncomment to check, data sorted recived
+                var elec = {
+                    startDate,
+                    street,
+                    streetNmbr,
+                    startTime,
+                    endTime,
+                    endWork,
+                    note
+                };
+                elecList.push(elec);
 
-    };
-    if (!elElec) {
-        //console.log('No new entries for electrical!');   //uncomment to see, no data recived
-    }   
-    
-    if (elWater) {
-        var elementWater = $('#cphContent_cphSadrzaj_upVoda table tbody  span').filter(function () {
-            return $(this).text() === self.searchStreet;
+            });
+            //console.log("New entries for electrical!", elecList);     //uncomment to check, data sorted recived
+
+        };
+        if (!elElec) {
+            //console.log('No new entries for electrical!');   //uncomment to see, no data recived
+        }
+
+        if (elWater) {
+            var elementWater = $('#cphContent_cphSadrzaj_upVoda table tbody  span').filter(function () {
+                return $(this).text() === self.searchStreet;
             }).parent().parent();
             $(elementWater).each(function (jex, wEl) {
-	    var noteWater = "";
-            const $wEl = $(wEl);
-            let startDate = $wEl.prevAll('.datum').first().find('tr[class="datum"]>th>div').text().trim();
-            let street = $wEl.find('span[id*="Label2"]').text();
-            let streetNmbr = $wEl.find('span[id*="Label7"]').text();
-            let startTime = $wEl.find('span[id*="Label8"]').text();
-            let endTime = $wEl.find('span[id*="Label9"]').text();
-            let endWork = $wEl.find('span[id*="Label10"]').text();
-            let noteCheck = $wEl.children().children().first()[0];
-            
-			if (noteCheck) {
-                var noteWater = $wEl.nextUntil().next().first('.tableNapomena').find('td[class="tableNapomena"]>div>div').text().trim();
-            };
-            var water = {
-                startDate,
-                street,
-                streetNmbr,
-                startTime,
-                endTime,
-                endWork,
-		noteWater
-            };
+                var noteWater = "";
+                const $wEl = $(wEl);
+                let startDate = $wEl.prevAll('.datum').first().find('tr[class="datum"]>th>div').text().trim();
+                let street = $wEl.find('span[id*="Label2"]').text();
+                let streetNmbr = $wEl.find('span[id*="Label7"]').text();
+                let startTime = $wEl.find('span[id*="Label8"]').text();
+                let endTime = $wEl.find('span[id*="Label9"]').text();
+                let endWork = $wEl.find('span[id*="Label10"]').text();
+                let noteCheck = $wEl.children().children().first()[0];
 
-            waterList.push(water);
-           
+                if (noteCheck) {
+                    var noteWater = $wEl.nextUntil().next().first('.tableNapomena').find('td[class="tableNapomena"]>div>div').text().trim();
+                };
+                var water = {
+                    startDate,
+                    street,
+                    streetNmbr,
+                    startTime,
+                    endTime,
+                    endWork,
+                    noteWater
+                };
+
+                waterList.push(water);
+
+            });
+            //   console.log('New entries for water!',waterList);   //uncomment to see, data sorted recived
+        };
+        if (!elWater) {
+            //   console.log('No new entries for water!');   //uncomment to see, no data recived
+        }
+       
+
+        self.sendSocketNotification('POWER AND WATER OUTAGES', {
+            waterList: waterList,
+            elecList: elecList
         });
-	 //   console.log('New entries for water!',waterList);   //uncomment to see, data sorted recived	
-    };
-    if (!elWater) {
-        //   console.log('No new entries for water!');   //uncomment to see, no data recived
-    	}    
-    //console.log(waterList);
-	
-    self.sendSocketNotification('POWER AND WATER OUTAGES', {waterList:waterList,elecList:elecList});
-    
-   
-    await browser.close();
+
+        await self.browser.close();
+    } catch (error) {
+        console.error('getData(): ' + error);
+
+    }
 };
